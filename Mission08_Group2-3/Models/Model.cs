e090487d4c6f8e544887f2b20fbd064725839470
Charlotte Griffin <charlottegriffin11@gmail.com>
2024-02-26 09:30:13 -0700

    Add view

--- Mission08_Group2-3/Models/Model.cs
+++ Mission08_Group2-3/Models/Model.cs
@@ -1,7 +1,10 @@
-﻿namespace Mission08_Group2_3.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Mission08_Group2_3.Models
 {
     public class Model
     {
+        // DELETE ME WHEN DONE (or edit idc)
     }
-    // DELETE ME WHEN DONE (or edit idc)
 }
