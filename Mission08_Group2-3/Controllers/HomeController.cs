2532b6e7947c7fbe07dd46c0e22c650c1cc50409
jmcguire001 <120243972+jmcguire001@users.noreply.github.com>
2024-02-26 21:05:37 -0700

    Updated HomeController.cs to include ITaskRepository

--- Mission08_Group2-3/Controllers/HomeController.cs
+++ Mission08_Group2-3/Controllers/HomeController.cs
@@ -6,12 +6,15 @@ namespace Mission08_Group2_3.Controllers
 {
     public class HomeController : Controller
     {
-        public HomeController()
+        private ITaskRepository _repo;
+        public HomeController(ITaskRepository temp)
         {
-
+            _repo = temp;
         }
         public IActionResult Index()
         {
+            ViewBag.Tasks = _repo.TempTask.FirstOrDefault(x => x.TaskId == "Criteria Here");
+
             return View();
         }
     }
