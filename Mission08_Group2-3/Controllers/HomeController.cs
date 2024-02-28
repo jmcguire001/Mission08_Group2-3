ef03c62571ee58a6c86ec328a4d14e77ee71411b
jmcguire001 <120243972+jmcguire001@users.noreply.github.com>
2024-02-27 23:03:50 -0700

    Updated the HomeController to include add, edit, and delete actions

--- Mission08_Group2-3/Controllers/HomeController.cs
+++ Mission08_Group2-3/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Mission08_Group2_3.Models;
 using System.Diagnostics;
+using System.Reflection.PortableExecutable;
 
 namespace Mission08_Group2_3.Controllers
 {
@@ -15,21 +17,101 @@ namespace Mission08_Group2_3.Controllers
             _repo = temp;
         }
 
-        public IActionResult Add()
-        {
-            return View();
-        }
-
-        public IActionResult QuadrantView()
-        {
-            return View();
-        }
         public IActionResult Index()
         {
             // This is temporary; updated this to actually show stuff later
             ViewBag.Tasks = _repo.TempTask.FirstOrDefault(x => x.TaskId == "Criteria Here");
 
             return View();
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            // Linq query to get all tasks
+            ViewBag.Categories = _repo.Categories.ToList();
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(Task response)
+        {
+            // Check validations
+            if (ModelState.IsValid)
+            {
+                _repo.Tasks.Add(response);
+                _repo.SaveChanges();
+
+                return View("Confirm", response); // WE NEED A CONFIRMATION PAGE
+            }
+            else
+            {
+                ViewBag.Categories = _repo.Categories.ToList(); // Have to pass the view bag if it's not valid
+
+                return View(response);
+            }
+        }
+
+        public IActionResult QuadrantView()
+        {
+            ViewBag.Categories = _repo.Categories.ToList();
+
+            // This is a LINQ query that will return a list of movies from the Movie database
+            var task = _repo.Tasks
+                .OrderBy(x => x.TaskName).
+                ToList();
+
+            return View(task);
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id) // Parameter name needs to match the asp-action, which is id
+        {
+            Task edit = _repo.Tasks.Single(x => x.TaskId == id); // This is a LINQ query that will return a single movie from the database
+            ViewBag.Categories = _repo.Categories.ToList();
+
+            return View("Add", edit);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Task update)
+        {
+            // Check validations for updating
+            if (ModelState.IsValid)
+            {
+                _repo.Movies.Update(update);
+                _repo.SaveChanges();
+
+
+                return RedirectToAction("QuadrantView"); // Redirects to the Collection view
+            }
+            else
+            {
+                ViewBag.Categories = _repo.Categories.ToList(); // Have to pass the view bag if it's not valid
+
+                return View("Add", update);
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            // Make sure we get the proper record to send to confirmation
+            var delete = _repo.Tasks.Single(x => x.TaskId == id);
+
+            return View(delete);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Task remove)
+        {
+            // Delete the proper record
+            _repo.Tasks.Remove(remove);
+            _repo.SaveChanges();
+
+            // Redirect to the Collection view after deletion
+            return RedirectToAction("QuadrantView");
         }
     }
 }
