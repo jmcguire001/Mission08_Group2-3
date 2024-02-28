099693e238310ee8766a5ebeec2dc063c2a8ec56
aforbush1 <aforbush17@gmail.com>
2024-02-28 09:13:08 -0700

    Added Methods and commented code

--- Mission08_Group2-3/Models/ITaskRepository.cs
+++ Mission08_Group2-3/Models/ITaskRepository.cs
@@ -1,9 +1,16 @@
 namespace Mission08_Group2_3.Models
 {
-    public interface ITaskRepository
+    public interface ITaskRepository // Interface which is a template for all of the classes. 
     {
-        List<Task> Tasks { get; }
+        
+        List<Task> Tasks { get; } // List containing items of type 'Task'
+        List<Category> Categories { get; } // List containing items of type 'Category'
 
-        public void AddTask(Task task);
+        
+        public void AddTask(Task task); //Method that takes parameter of task of type 'Task' and returns nothing. Classes implementing this interface are required to define how to add a Task object
+
+        public void UpdateTask(Task task); // Method for updating tasks to the views (This is for when the user edits)
+
+        public void DeleteTask(Task task); // Method for deleting tasks from the views (This is for when the user deletes)
     }
 }
