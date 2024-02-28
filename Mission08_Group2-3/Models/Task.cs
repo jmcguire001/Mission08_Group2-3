980c263e81b2f5d40c567c8dde7c27795524e1f4
aforbush1 <aforbush17@gmail.com>
2024-02-28 09:29:21 -0700

    Added Quadrant Field to Database

--- Mission08_Group2-3/Models/Task.cs
+++ Mission08_Group2-3/Models/Task.cs
@@ -8,18 +8,20 @@ namespace Mission08_Group2_3.Models
         [Key]
         [Required]
         public int TaskId { get; set; }
+
         [Required]
         public string TaskName { get; set; }
 
         public string? DueDate { get; set; }
-        [Required]
-        public int Quadrant { get; set; }
 
         [ForeignKey("CategoryId")]
         public int? CategoryId { get; set; }
-        public Category? Category { get; set; } 
 
+        public Category? Category { get; set; }
+        
         public bool? Completed { get; set; }
 
+        [Required]
+        public int Quadrant { get; set; }
     }
 }
